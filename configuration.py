8d0bedebefbeb2c73ea4020f562a109cf67105b1
Vioshat <violettashatkova@yandex.ru>
2024-01-20 19:18:51 +0100

    «загружено”

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE = 'https://d2474f70-eb06-4a2b-9fb5-3c4777690cbd.serverhub.praktikum-services.ru'
+CREATE_ORDERS = '/api/v1/orders'
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Виолетта",
+    "lastName": "Шаткова",
+    "address": "Пионерская 24",
+    "metroStation": 4,
+    "phone": "+7 921 777 77 77",
+    "rentTime": 4,
+    "deliveryDate": "2024-03-16",
+    "comment": "Комментарий",
+    "color": [
+        "BLACK"
+    ]
+}
